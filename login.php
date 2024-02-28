18a6e46173f38ffa75dc51f3697960ebf48d82a1
[Ujjwal Shrestha] <[shresthamessi20@gmail.com]>
2024-02-28 10:23:17 +0545

    initial message

--- login.php
+++ login.php
@@ -10,10 +10,10 @@
 <body>
     <div class="container">
         <div class="form-box"><h2>Login</h2><br>
-        <form>
+        <form action="logincheck.php" method="post" onsubmit = "return validation()">
             <div class="c">
-            <input class="username" type="text" placeholder="Username"><br><br>
-            <input class="password" type="password" placeholder="Password"><br>
+            <input class="username" type="text" name="username" placeholder="Username"><br><br>
+            <input class="password" type="password" id="password" name="password" placeholder="Password"><br><
         </div>
         
         <p class="forgetpw"><a href="#">Forget password?</a></p><br>
@@ -22,6 +22,27 @@
         </form>   
     </div>
     </div>
-
+    <script>  
+            function validation()  
+            {  
+                var id=document.f1.user.value;  
+                var ps=document.f1.pass.value;  
+                if(id.length=="" && ps.length=="") {  
+                    alert("User Name and Password fields are empty");  
+                    return false;  
+                }  
+                else  
+                {  
+                    if(id.length=="") {  
+                        alert("User Name is empty");  
+                        return false;  
+                    }   
+                    if (ps.length=="") {  
+                    alert("Password field is empty");  
+                    return false;  
+                    }  
+                }                             
+            }  
+        </script>  
 </body>
 </html>
