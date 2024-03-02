fe7615eae96a209ecde86c5c37579bb769c43b94
[Ujjwal Shrestha] <[shresthamessi20@gmail.com]>
2024-03-02 15:18:44 +0545

    css milako xa

--- contact.php
+++ contact.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/contactstyle.css"> 
+    <link rel="stylesheet" href="css/contactstyle.css">
+    <style>
+        .notice {
+            background-image: url(photo/bgdoctor.jpg) !important;
+        }
+    </style>
 </head>
+
 <body>
     <?php
     include_once("nav.php");
     ?>
-    <div class="image">
-        <img src="photo/photo3.jpg" alt="wait" width="100%" height="300px" >
-        <p>Contact For Any Query</p>
+    <div class="notice">
+        <div class="photo">
+            <h1>Contact For Any Query</h1>
+        </div>
     </div>
     <div class="check">
-    <img src="photo/photo3.jpg" alt="wait" width="100%" height="300px">
         <div class="background">
             <form action="" method="POST">
                 <table>
@@ -24,12 +31,17 @@
                         <td><input type="text" name="Name" id="Name" placeholder="Enter your name" required></td>
                     </tr>
                     <tr>
+                        <td><label for="email">E-mail: </label></td>
+                        <td><input type="text" name="email" id="email" placeholder="Enter your name" required></td>
+                    </tr>
+                    <tr>
                         <td><label for="Sub">Subject: </label></td>
                         <td><input type="text" name="sub" id="sub" placeholder="Subject" required></td>
                     </tr>
                     <tr>
                         <td><label for="textbox">Message: </label></td>
-                        <td><textarea name="comment" id="" cols="30" rows="10"  placeholder="Message" required></textarea></td>
+                        <td><textarea name="comment" id="" cols="30" rows="10" placeholder="Message"
+                                required></textarea></td>
                     </tr>
                     <tr>
                         <td colspan="2"><input type="submit" value="Submit"></td>
@@ -37,10 +49,11 @@
                 </table>
             </form>
         </div>
-       
+
     </div>
     <?php
     include_once("footer.php");
     ?>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -9,12 +9,12 @@ include_once("nav.php");
                 practice.</p>
         </div>
         <div class="par1">
-            <img src="photo/photo1.jpg" alt="" >
+            <img src="photo/photo1.jpg" alt="">
         </div>
     </div>
     <div class="main2 ">
         <div class="par1">
-            <img src="photo/photo2.jpg" alt="" >
+            <img src="photo/photo2.jpg" alt="">
         </div>
         <div class="par1">
             <h1>Ease Scheduling pains</h1>
@@ -22,7 +22,7 @@ include_once("nav.php");
                 minutes by phone * to just few clicks
 
             </p>
-            <div > <button class="button">Create your Account</button></div>
+
         </div>
     </div>
     <div class="main4">
@@ -43,13 +43,13 @@ include_once("nav.php");
         <div class="par1">
             <h1>Friendly Doctors</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab quaerat fugit
-            ipsum possimus, debitis quos, fugiat perferendis consequatur accusantium nemo quae commodi
-            placeat voluptas deleniti error, neque amet ea officia?</p>
+                ipsum possimus, debitis quos, fugiat perferendis consequatur accusantium nemo quae commodi
+                placeat voluptas deleniti error, neque amet ea officia?</p>
         </div>
     </div>
     <div class="main4">
         <div class="par1">
-            <h1>i love you ujjwal. I love you too!</h1>
+            <h1>Up to date Patient Information</h1>
             <p>A patients Customer Profile automatically updates when they book an appointment.Centralize contact
                 information and attach notes for fast access before appointment . your online doctor scheduling app
                 allows you to digitally back-up patient health records and treatment recommendation.</p>
@@ -60,7 +60,7 @@ include_once("nav.php");
     </div>
     <div class="main5">
         <div class="par1">
-            <img src="photo/photo4.jpg" alt="" >
+            <img src="photo/photo4.jpg" alt="">
         </div>
         <div class="par1">
             <h1>Accept medical fees online.</h1>
@@ -74,21 +74,20 @@ include_once("nav.php");
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quidem sint commodi aliquid deserunt
                 libero, aspernatur ut, molestiae sapiente accusamus delectus itaque. Suscipit quam id, veritatis
                 impedit cumque sequi reiciendis.</p>
-            <div> <button class="button" > Get started Now</button></div>
+
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div class="par1">
-        <img src="photo/photo5.jpg" alt="">
+            <img src="photo/photo5.jpg" alt="">
         </div>
-        
+
     </div>
 
     <div class="notice">
         <div class="photo">
-        <h1>Make the magic happen,we'll make sure it happens on time.</h1>
-        <button class="button" > Sign me up</button>  
+            <h1>Make the magic happen,we'll make sure it happens on time.</h1>
         </div>
-        
+
     </div>
 </div>
 <?php
